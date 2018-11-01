1ab0a5ba9e29bfea9f2fa1e20378f488b149b202
Zihan Zhang <769492047@qq.com>
2018-11-01 21:00:24 +0000

    Reset datastore to guarantee test isolation

--- test/routes/courses-test.js
+++ test/routes/courses-test.js
@@ -1,13 +1,25 @@
-process.env.NODE_ENV = 'development';
+process.env.NODE_ENV = 'test';
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../../bin/www');
 let expect = chai.expect;
-
+let datastore = require('../../models/courses');
 chai.use(chaiHttp);
 let _ = require('lodash' );
 
 describe('Courses', function (){
+    beforeEach(function(done){
+        var newData=new datastore({
+            name:"math",
+            type:"P",
+            size:16,
+            room:"A01",
+            time:9.15
+        });
+        newData.save(function(err){
+            done();
+        });
+    });
     describe('GET /courses',  () => {
         it('should return all the courses in an array', function(done) {
             chai.request(server)
@@ -15,14 +27,14 @@ describe('Courses', function (){
                 .end(function(err, res) {
                     expect(res).to.have.status(200);
                     expect(res.body).to.be.a('array');
-                    expect(res.body.length).to.equal(4);
+                    expect(res.body.length).to.equal(1);
                     let result = _.map(res.body, (course) => {
                         return { name: course.name,
                             type: course.type }
                     });
                     expect(result).to.include( { name: "math", type: "P"  } );
                     // expect(result).to.include( { name: "english", type: "P"  } );
-                    // datastore.collection.drop();
+                    datastore.collection.drop();
                     done();
                 });
 
